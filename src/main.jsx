191ea391e0d761477145fad6f3a58f42fedad382
Rubens Tadeu <rubenstadeumoura@gmail.com>
2024-05-27 14:01:44 -0300

    Distribuição de grid da Home

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Details } from './pages/Details/index.jsx';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/global.js';
 
 import theme from './styles/theme.js';
 
+import { Home } from './pages/Home';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Details />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>
 )
