1a52639cb8bdf057efceea2d50d398980bd0099f
Rubens Tadeu <rubenstadeumoura@gmail.com>
2024-05-27 22:10:09 -0300

    Formulário SignIn

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,9 +1,38 @@
-import { Container } from './styles';
+import { FiMail, FiLock} from 'react-icons/fi';
 
-export function SingIn() {
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
+import { ButtonText } from '../../components/ButtonText';
+
+import { Container, Form } from "./styles";
+
+export function SignIn() {
   return(
     <Container>
-      
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+
+        <h2>Faça seu login</h2>
+
+        <Input 
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+        />
+
+        <Input 
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+        />
+
+        <Button title="Entrar" />
+
+        <a href="#">
+          Criar conta
+        </a>
+      </Form>
     </Container>
   );
 }
