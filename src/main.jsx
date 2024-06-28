322e8f511667c3befb3c57c4ca70481882cec62f
rubenstadeum <rubenstadeumoura@gmail.com>
2024-06-28 11:45:12 -0300

    add interface New

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import GlobalStyles from './styles/global.js';
 
 import theme from './styles/theme.js';
 
-import { Profile } from './pages/Profile/index.jsx';
+import { New } from './pages/New/index.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Profile />
+      <New />
     </ThemeProvider>
   </React.StrictMode>
 )
