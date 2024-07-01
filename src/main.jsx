584a3a4be360681be6a9d8175352d56faddff8a6
rubenstadeum <rubenstadeumoura@gmail.com>
2024-07-01 18:28:18 -0300

    add Routes

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import GlobalStyles from './styles/global.js';
 
 import theme from './styles/theme.js';
 
-import { New } from './pages/New/index.jsx';
+import { Routes } from './routes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <New />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>
 )
